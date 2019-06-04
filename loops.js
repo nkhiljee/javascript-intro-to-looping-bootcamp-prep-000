c6490cad5c3c18c9bada3133a34a671728c6a034
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-04 00:30:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,9 +19,14 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(num) {
-  
-  
-  do {
-    []
-  }
+ var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
 }
